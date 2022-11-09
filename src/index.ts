e7c071c993bd6d51bfdfca669171b3f43b961290
JoaoOliveira <joao.oliveira0117@hotmail.com>
2022-11-08 21:34:12 -0300

    fix cors issue

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express'
+import cors from 'cors'
 import dbConnect from './config/db'
 import router from './routes'
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.use('/', router)
@@ -10,5 +12,5 @@ app.use('/', router)
 dbConnect().catch((err) => console.log(err))
 
 app.listen(3000, () => {
-  console.log('Listening...')
+  console.log('App up and running at http://localhost:3000')
 })
